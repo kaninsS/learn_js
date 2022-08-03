b5a166eb35482d9c0d28723825336082220dcaef
kaninsS <78402640+kaninsS@users.noreply.github.com>
2022-08-04 04:50:46 +0700

    learn_v1

---
+++ learn_js.js
@@ -0,0 +1,152 @@
+// JavaScript can "dinnerHTMLisplay" data in different ways:
+
+
+//(1)OUTPUT
+
+// Writing into the HTML output using document.write().
+// document.write("Hello World!");
+// document.write(10+9);
+// document.write("<h2>Hello World!</h2><p>Have a nice day!</p>");
+
+// Writing into an HTML element, using innerHTML.
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "New";
+//   }
+// document.getElementById("data").innerHTML = 5000 + 6;
+
+// Writing into an alert box, using window.alert().
+// window.alert("Hello mama");
+
+// Writing into the browser console, using console.log().
+// console.log("test");
+
+
+//(2)VARIABLE - var, let, const
+// How to create variables:
+// var x;
+// let y;
+// How to use variables:
+// x = 5;
+// y = 6;
+// let z = x + y;
+// const price1 = 5;
+// const price2 = 6;
+// let total = price1 + price2;
+
+//(3)VALUE - 2 type Literals & Variables(store data).
+// let x;
+// x = 6;
+
+//(4)CASE SENSITIVE 
+// let lastname, lastName;
+// lastName = "Doe";
+// lastname = "Peterson";
+
+//(5)Camel Case
+// Underscore:
+// first_name, last_name, master_card, inter_city.
+
+// Upper Camel Case (Pascal Case):
+// FirstName, LastName, MasterCard, InterCity.
+
+// Lower Camel Case: (popular)
+// firstName, lastName, masterCard, interCity.
+
+//(6)Operators 
+//https://www.w3schools.com/js/js_operators.asp
+
+
+
+//(7)Arithmetic
+//https://www.w3schools.com/js/js_arithmetic.asp
+
+//(8)Assignment
+//https://www.w3schools.com/js/js_assignment.asp
+
+//(9)Data Types - String Number Boolean Array Object
+// let length 16;                               // Number
+// let lastName = "Johnson";                      // String
+// let x = {firstName:"John", lastName:"Doe"};    // Object
+
+//NOTE "" and '' in string
+// let answ= er1 = "It's alright";
+// let answer2 = "He is called 'Johnny'";
+// let answer3 = 'He is called "Johnny"';
+
+//(9.1)Booleans
+
+//(9.2)Array
+//const cars = ["Saab", "Volvo", "BMW"];
+
+//(9.3)Object
+//const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+//typeof();
+
+//(10)Function - https://www.w3schools.com/js/js_functions.asp
+// function toCelsius(fahrenheit) {
+//     return (5/9) * (fahrenheit-32);
+//   }
+//   let x = toCelsius(77);
+//   let text = "The temperature is " + x + " Celsius";
+//   document.getElementById("c_result").innerHTML = toCelsius(77);
+
+//(11)Comparison Operators - https://www.w3schools.com/js/js_comparisons.asp
+
+//(12)If  & else
+// const time = new Date().getHours();
+// let greeting;
+// if (time < 10) {
+//   greeting = "Good morning";
+// } else if (time < 20) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+// document.getElementById("demo").innerHTML = greeting;
+
+//(13)Switch - https://www.w3schools.com/js/js_switch.asp
+// switch (new Date().getDay()) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+
+//(14)Loop - https://www.w3schools.com/js/js_loop_for.asp
+// for - loops through a block of code a number of times
+// for/in - loops through the properties of an object
+// for/of - loops through the values of an iterable object
+// while - loops through a block of code while a specified condition is true
+// do/while - also loops through a block of code while a specified condition is true
+
+//(15)Method - https://www.w3schools.com/jsref/jsref_obj_array.asp
+
+//(16)JSON
+
+
+
+//console.log(text);
+// document.write(x);
+
